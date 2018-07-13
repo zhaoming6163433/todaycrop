ee1a0e3c8357c0083049816424644be9e9ca4d93
zhaoming <zhaoming6163433@163.com>
2018-07-13 16:49:42 +0800

    cs

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ export default new Router({
   mode:'history',
   routes: [
     {//首页
-      path: '/',
+      path: '/todaypocket',
       name: 'home',
       component: home,
       children: [
